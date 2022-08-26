1972646946dc56f147d78930ee73bbe22849ba4a
Eduardo Lussi <eduardolussi@gmail.com>
2022-08-26 18:02:31 -0300

    date bug

--- servidor-fog/src/controllers/PayloadController.js
+++ servidor-fog/src/controllers/PayloadController.js
@@ -1,7 +1,7 @@
 const Payload = require('../models/Payload');
 const Sensor = require('../models/Sensor');
 const PayloadAttribute = require('../models/PayloadAttribute');
-const SensorType = require('../models/SensorType');
+const AttributeSensorType = require('../models/AttributeSensorType');
 
 function linspace(start, stop, num, endpoint = true) {
     const div = endpoint ? (num - 1) : num;
@@ -24,18 +24,21 @@ module.exports = {
 
         const { system_id } = await Sensor.findOne({ _id: sensor_id });
         req.io.emit(`payload${system_id}`, { payloadAttributes, sensor_id });
-        console.log(payloadAttributes)
         return res.json(payload);
     },
 
     async show(req, res) {
-        const { system_id, data_from, data_to, attribute_id } = req.headers;
+        const { system_id, attribute_id } = req.headers;
+        let { data_from, data_to } = req.headers;
 
-        const sensor_types = await SensorType.find({ attribute_id });
+        if (data_from) data_from += "-03:00";
+        if (data_to) data_to += "-03:00";
+
+        const sensor_types = await AttributeSensorType.find({ attribute_id });
 
         let sensors = [];
         for (let i = 0; i < sensor_types.length; i++) {
-            sensors.push.apply(sensors, await Sensor.find({ sensor_type_id: sensor_types[i]._id,
+            sensors.push.apply(sensors, await Sensor.find({ sensor_type_id: sensor_types[i].sensor_type_id,
                                                             system_id }));
         }
 
@@ -44,9 +47,11 @@ module.exports = {
             const sensorPayloads = await Payload.find({ sensor_id: sensors[i]._id, 
                                                         data: { $gte: data_from, $lte: data_to } });
             
-            let samples
-            if (payloads.length > 100)
+            let samples = [];
+            if (sensorPayloads.length > 100)
                 samples = linspace(0, sensorPayloads.length-1, 100);
+            else if (sensorPayloads.length == 1)
+                samples = [0];
             else
                 samples = linspace(0, sensorPayloads.length-1, sensorPayloads.length);
             
